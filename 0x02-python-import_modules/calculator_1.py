0083dc59183b2e681a196856182acd4b4b85bd74
Kwanzu-J <jkwanzu105@gmail.com>
2023-08-11 14:40:50 -0700

    1. My first toolbox!

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+def sub(a, b):
+    """
+    Subtraction function
+    Args:
+        a - first integer
+        b - second integer
+    Returns:
+        return value a - b
+    """
+    return a - b
+
+def add(a, b):
+    """
+    Addition function
+    Args:
+        a - first integer
+        b - second integer
+    Returns:
+        return value a + b
+    """
+    return a + b
+
+def mul(a, b):
+    """
+    multiplication function
+    Args:
+        a - first integer
+        b - second integer
+    Returns:
+        return value a * b
+    """
+    return a * b
+
+def div(a, b):
+    """
+    Divition function
+    Args:
+        a - first integer
+        b - second integer
+    Returns:
+        return value a / b
+    """
+    return a / b
