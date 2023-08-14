cebc896434ba09d3acf94f374515a106c73d160a
Kwanzu-J <jkwanzu105@gmail.com>
2023-08-14 15:06:34 -0700

    2. Replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,7 +4,7 @@ def replace_in_list(my_list, idx, element):
     low = 0
     high = len(my_list)
     if idx < low or idx > high:
-        return none
+        return None
     else:
         my_list[idx] = element
         return my_list
